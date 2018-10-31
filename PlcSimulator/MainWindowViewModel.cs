5464cfd29f3162e3c94ae92b0f5856fdd7dd52d8
AyaKujirai <kujiraia@lilymedtech.com>
2018-10-31 14:38:38 +0900

    ViewModelとModelを追加

---
+++ PlcSimulator/MainWindowViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+
+namespace PlcSimulator
+{
+    using System.Windows.Input;
+
+    public class MainWindowViewModel : BindableBase
+    {
+        private readonly Model model;
+
+        private double leftValue;
+
+        private double rightValue;
+
+        private double answerValue;
+
+        public MainWindowViewModel()
+        {
+            this.model = new Model();
+            this.CalcCommand = new DelegateCommand(this.CalcExecute);
+        }
+
+        public double LeftValue
+        {
+            get => this.leftValue;
+            set => this.SetProperty(ref this.leftValue, value);
+        }
+
+        public double RightValue
+        {
+            get => this.rightValue;
+            set => this.SetProperty(ref this.rightValue, value);
+        }
+
+        public double AnswerValue
+        {
+            get => this.answerValue;
+            set => this.SetProperty(ref this.answerValue, value);
+        }
+
+        public ICommand CalcCommand { get; set; }
+
+        private bool CanCalcExecute() => true;
+
+        private void CalcExecute() => this.AnswerValue = this.model.Calc(this.LeftValue, this.RightValue);
+    }
+}
---
+++ PlcSimulator/Model.cs
@@ -0,0 +1,14 @@
+﻿namespace PlcSimulator
+{
+    using Prism.Mvvm;
+
+    public interface IModel
+    {
+        double Calc(double x, double y);
+    }
+
+    public class Model : IModel
+    {
+        public double Calc(double x, double y) => x + y;
+    }
+}
