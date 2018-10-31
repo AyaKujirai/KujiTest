2c7af32d0122042b2f51995cc5f12448806c686a
AyaKujirai <kujiraia@lilymedtech.com>
2018-10-31 14:26:26 +0900

    変更

--- PlcSimulator/App.xaml.cs
+++ PlcSimulator/App.xaml.cs
@@ -1,17 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace PlcSimulator
 {
+    using Prism.Ioc;
+
     /// <summary>
     /// App.xaml の相互作用ロジック
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        protected override Window CreateShell() => this.Container.Resolve<MainWindow>();
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.Register<MainWindow>();
+        }
     }
 }
