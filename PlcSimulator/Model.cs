00b1d3f11a70db8d31ff5aff258231def39dd97f
AyaKujirai <kujiraia@lilymedtech.com>
2018-11-07 17:29:09 +0900

    競合を修正

--- PlcSimulator/Model.cs
+++ PlcSimulator/Model.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿namespace PlcSimulator
+namespace PlcSimulator
 {
     using Prism.Mvvm;
 
@@ -11,21 +10,5 @@
     public class Model : IModel
     {
         public double Calc(double x, double y) => x + y;
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PlcSimulator
-{
-    public class Model
-    {
-        public double Calc(double x, double y)
-        {
-            return x + y;
-        }
->>>>>>> d4fc21dc17c2bbf07daf17e4e99d1af1a9d868e0
     }
 }
