f7d101c57eb342691e388590c8ba78555f709b6b
AyaKujirai <kujiraia@lilymedtech.com>
2018-11-07 11:02:39 +0900

    単体テスト追加

---
+++ PlcSimulatorTests/ModelTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PlcSimulator.Tests
+{
+    [TestClass]
+    public class ModelTests
+    {
+        [TestMethod]
+        public void CalcTest()
+        {
+            var target = new Model();
+            double x = 1d, y = 2d;
+            Assert.AreEqual(3d, target.Calc(x, y));
+        }
+    }
+}
